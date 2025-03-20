c8d3fc8d61ef5fe578d58bf1a8fd6b44cd5c6f7c
CrisMarin1211 <alca81411@gmail.com>
2025-03-20 17:36:07 -0500

    feact

--- Lab3-Progra-Web/src/Components/CatImage.jsx
+++ Lab3-Progra-Web/src/Components/CatImage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const CatImage = ({ imageUrl }) => {
-	// <-- Prop corregida a "imageUrl"
 	return <img src={imageUrl} alt='Minino' style={{ maxWidth: '100%' }} />;
 };
 
